5823e8f379a5ed0f95090ec673abf58871a59505
AnnaMizgailo <102236391+AnnaMizgailo@users.noreply.github.com>
2023-02-25 16:15:35 +0300

    first commit

---
+++ js/hero.js
@@ -0,0 +1,22 @@
+let hero = {
+    x: 290,
+    y: 290,
+    move: function(x = this.x, y = this.y) {
+        // check area is out
+        if (x < 10 || x > 590) return;
+        if (y < 10 || y > 690) return;
+
+        this.clear();
+
+        this.x = x;
+        this.y = y;
+        context.beginPath();
+        var img = new Image();  
+        img.src = '../img/frisk.jpg'; 
+        context.drawImage(img, this.x, this.y, 30, 30); 
+        context.closePath();
+    },
+    clear: function() {
+        context.clearRect(this.x, this.y, 30, 30);
+    }
+};
---
+++ js/index.js
@@ -0,0 +1,4 @@
+let canvas = document.getElementsByTagName("canvas")[0];
+canvas.width = "600";
+canvas.height = "600";
+let context = canvas.getContext("2d");
